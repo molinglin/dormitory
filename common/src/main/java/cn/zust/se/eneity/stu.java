7331b9b5269c463e4b402722f676415433cf4ef7
twj <3223362601@qq.com>
2022-05-30 13:23:33 +0800

    twj

---
+++ common/src/main/java/cn/zust/se/eneity/stu.java
@@ -0,0 +1,15 @@
+package cn.zust.se.eneity;
+
+import lombok.Data;
+
+@Data
+public class stu {
+    private Integer id;
+    private String uid;
+    private String name;
+    private String gender;
+    private String phone;
+    private String college;
+    private String major;
+    private String classes;
+}
--- common/src/main/java/cn/zust/se/stu.java
+++
@@ -1,4 +0,0 @@
-package cn.zust.se;
-
-public class stu {
-}
