89de4f68265e4471c2d04043b2e2999d1fda1c78
twj <3223362601@qq.com>
2022-05-30 13:28:10 +0800

    twj

--- common/src/main/java/cn/zust/se/eneity/master.java
+++ common/src/main/java/cn/zust/se/eneity/master.java
@@ -1,4 +1,12 @@
 package cn.zust.se.eneity;
 
+import lombok.Data;
+
+@Data
 public class master {
+    private Integer id;
+    private String did;
+    private String name;
+    private String phone;
+    private Integer building_id;
 }
