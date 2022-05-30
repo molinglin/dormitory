623fa8987b60ac8b5110564d2306ee24db077a3d
feng <929055273@qq.com>
2022-05-30 21:06:58 +0800

    5

--- student/src/main/java/cn/zust/se/StuApplication.java
+++ student/src/main/java/cn/zust/se/StuApplication.java
@@ -2,8 +2,10 @@ package cn.zust.se;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class StuApplication {
     public static void main(String[] args) {
         SpringApplication.run(StuApplication.class,args);
