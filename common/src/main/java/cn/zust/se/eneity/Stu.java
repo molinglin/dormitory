7c68d37b0dc4a2d261d247bec6fe1fe5a231ea3a
feng <929055273@qq.com>
2022-05-30 17:01:08 +0800

    1

--- common/src/main/java/cn/zust/se/eneity/Stu.java
+++ common/src/main/java/cn/zust/se/eneity/Stu.java
@@ -1,9 +1,15 @@
 package cn.zust.se.eneity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
 
 @Data
-public class Stu {
+@AllArgsConstructor
+@NoArgsConstructor
+public class Stu implements Serializable {
     private Integer id;
     private String uid;
     private String name;
