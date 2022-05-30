7e06dc13072c7987d7052fbc7b0702274548fc52
twj <3223362601@qq.com>
2022-05-30 14:34:55 +0800

    twj

--- user/src/main/java/cn/zust/se/dao/userDao.java
+++ user/src/main/java/cn/zust/se/dao/userDao.java
@@ -1,10 +1,13 @@
 package cn.zust.se.dao;
 
+import cn.zust.se.eneity.User;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
 
 @Mapper
 @Repository
 public interface userDao {
-    public
+    @Select("select * from user where username=#{username}")
+    User login(String username,String password);
 }
