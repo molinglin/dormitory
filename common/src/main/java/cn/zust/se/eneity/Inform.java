acf34bfc00b0cf62d6a3f613b62b765d45e7de7b
twj <3223362601@qq.com>
2022-06-01 16:46:22 +0800

    twj

---
+++ common/src/main/java/cn/zust/se/eneity/Inform.java
@@ -0,0 +1,13 @@
+package cn.zust.se.eneity;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class Inform {
+    public Integer id;
+    public Date time;
+    public String publisher;
+    public String content;
+}
