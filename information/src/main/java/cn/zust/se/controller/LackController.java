c8f0458fbdfcdb19c49ff29b0a9bd6c04272993e
twj <3223362601@qq.com>
2022-06-04 15:21:22 +0800

    twj

---
+++ information/src/main/java/cn/zust/se/controller/LackController.java
@@ -0,0 +1,69 @@
+package cn.zust.se.controller;
+
+import cn.zust.se.eneity.CommonResult;
+import cn.zust.se.eneity.Lack;
+import cn.zust.se.service.LackService;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.util.Date;
+import java.util.List;
+
+@Api("缺寝管理接口")
+@RestController
+@RequestMapping("/lack")
+public class LackController {
+    @Resource
+    LackService lackService;
+    @ApiOperation("添加缺寝记录")
+    @PostMapping("/insertLack")
+    public CommonResult insertLack(String uid, Date time, Integer buildingid, String dormitory,String intro){
+        if(lackService.insertLack(uid, time, buildingid, dormitory,intro)==1){
+            return new CommonResult<>(200,"success",null);
+        }else return new CommonResult<>(400,"fail",null);
+    }
+
+    @ApiOperation("按时间次序查找所有记录")
+    @GetMapping("/selAllLack")
+    public CommonResult selAllLack(@RequestParam(defaultValue = "1",value = "pageNum")Integer pageNum){
+        PageHelper.startPage(pageNum,5);
+        List<Lack> lacks=lackService.selAllLack();
+        PageInfo<Lack> pageInfo=new PageInfo<>(lacks);
+        if(lacks.isEmpty()){
+            return new CommonResult(400,"fail",null);
+        }else {
+            return new CommonResult<>(200,"success",pageInfo.getList());
+        }
+    }
+
+    @ApiOperation("按时间次序与学号查找所有记录")
+    @GetMapping("/selLackByUid")
+    public CommonResult selLackByUid(@RequestParam(defaultValue = "1",value = "pageNum")Integer pageNum,String uid){
+        PageHelper.startPage(pageNum,5);
+        List<Lack> lacks=lackService.selLackByUid(uid);
+        PageInfo<Lack> pageInfo=new PageInfo<>(lacks);
+        if(lacks.isEmpty()){
+            return new CommonResult(400,"fail",null);
+        }else {
+            return new CommonResult<>(200,"success",pageInfo.getList());
+        }
+    }
+
+    @ApiOperation("按时间次序与楼宇查找所有记录")
+    @GetMapping("/selLackByB")
+    public CommonResult selLackByB(@RequestParam(defaultValue = "1",value = "pageNum")Integer pageNum,Integer buildingid){
+        PageHelper.startPage(pageNum,5);
+        List<Lack> lacks=lackService.selLackByBuilding(buildingid);
+        PageInfo<Lack> pageInfo=new PageInfo<>(lacks);
+        if(lacks.isEmpty()){
+            return new CommonResult(400,"fail",null);
+        }else {
+            return new CommonResult<>(200,"success",pageInfo.getList());
+        }
+    }
+
+}
