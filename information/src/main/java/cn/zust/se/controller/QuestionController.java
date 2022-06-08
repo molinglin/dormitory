36f76a1aad943a8c36172ad424a3fc281fc78093
twj <3223362601@qq.com>
2022-06-08 16:58:05 +0800

    twj

---
+++ information/src/main/java/cn/zust/se/controller/QuestionController.java
@@ -0,0 +1,12 @@
+package cn.zust.se.controller;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api("问卷调查")
+@RestController
+@RequestMapping("/question")
+public class QuestionController {
+}
